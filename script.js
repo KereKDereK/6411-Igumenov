d96ebbbc7bb1266fbbac3ac0853d4e79a59ce2d0
KereKDereK <75019623+KereKDereK@users.noreply.github.com>
2022-09-02 15:24:50 +0400

    issues fixed

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 function buttonClickHandler() {
+    document.getElementById("Input1").style.background = "lightgreen";
+    document.getElementById("Input2").style.backgroundColor = "lightgreen";
     let value1 = document.getElementById("Input1").value;
     let value2 = document.getElementById("Input2").value;
+    value1 = parseFloat(value1);
+    value2 = parseFloat(value2);
     let result = 0;
     let result1 = "OK";
-    if (!isNaN(parseFloat(value1)) && isFinite(value1) && !isNaN(parseFloat(value2)) && isFinite(value2))
+    if (!isNaN(value1) && isFinite(value1) && !isNaN(value2) && isFinite(value2))
     {
         switch (document.getElementById("select").value){
             case "1":
-                result = parseFloat(value1) + parseFloat(value2);
+                result = value1 + value2;
                 break;
         
             case "2":
-                result = parseFloat(value1) - parseFloat(value2);
+                result = value1 - value2;
                 break;
         
             case "3":
-                result = parseFloat(value1) * parseFloat(value2);
+                result = value1 * value2;
                 break;
         
             case "4":
-                if (parseFloat(value2) != 0)
+                if (value2 !== 0.0)
                 {
-                    result = parseFloat(value1) / parseFloat(value2);
+                    result = value1 / value2;
                 }
                 else
                 {
-                    result1 = "Error";
+                    document.getElementById("Input2").style.backgroundColor = "yellow";
+                    result1 = "Error. Second value is zero";
                 }
                 break;
         
@@ -36,15 +41,23 @@ function buttonClickHandler() {
 
     else
     {
-        result1 = "Error";
+        if (isNaN(parseFloat(value1))){
+            document.getElementById("Input1").style.backgroundColor = "yellow";
+            result1 = "Error. First value is not a number";
+        }
+        else
+        {
+            document.getElementById("Input2").style.backgroundColor = "yellow";
+            result1 = "Error. Second value is not a number";
+        }
     }
     
-    if (result1 == "OK")
+    if (result1 === "OK")
     {
-        document.getElementById("results").innerText = result;
+        document.getElementById("results").value += result + "\n";
     }
     else
     {
-        document.getElementById("results").innerText = result1;
+        document.getElementById("results").value += result1 + "\n";
     }
 }
